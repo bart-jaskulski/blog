d557591e04d889c48fd4a6cd6f3916aa1f564d38
Bart Jaskulski <bjaskulski@protonmail.com>
2023-11-19 21:15:48 +0100

    Use native datetime type
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,8 +15,13 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('content')
-            ->add('created')
-            ->add('updated')
+            ->add('created', options: [
+                'widget' => 'single_text'
+            ])
+            ->add('updated', options: [
+              'widget' => 'single_text'
+            ])
+            ->add('save', SubmitType::class)
         ;
     }
 
