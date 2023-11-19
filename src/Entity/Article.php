96f418747594edf7869bfbd6ca8b69b6f8c64f45
Bart Jaskulski <bjaskulski@protonmail.com>
2023-11-19 21:09:11 +0100

    Add Article entity constraints
    Signed-off-by: Bart Jaskulski <bjaskulski@protonmail.com>

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -15,9 +16,11 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank]
     private ?string $content = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
